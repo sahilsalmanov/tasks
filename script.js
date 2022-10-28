cd1a4df349eff4c9bd06788cefcc5961c1c6b653
sahilsalmanov <salmanovsahil7@gmail.com>
2022-10-28 14:29:36 +0400

    tasks

---
+++ script.js
@@ -0,0 +1,93 @@
+// const sentence = ['sense','make','all', 'will','This']
+// const sentence2 = []
+// function reverseArray(arr) {
+// for(let i = arr.length-1;i>=0;i--) {
+//     sentence2.push(arr[i])
+// }
+// console.log(sentence2)
+// }
+// reverseArray(sentence)
+
+
+//Tap1
+
+// const aliens = ['Blorgos','Glamyx','Wegord','SpaceKing']
+// function greetAliens(arr) {
+//     for(let i =0;i<arr.length;i++) {
+//     console.log(`Oh powerful  ${arr[i]}, we humans offer our unconditional surrender!`)
+//     }
+// }
+// greetAliens(aliens)
+
+// //Tap2
+
+
+// const coolStuff = [
+//     'gameboy',
+//     'skateboard',
+//         'backwards hat',
+//        'my room',
+//     'temporary tattoo'
+//       ]
+//       const myStuff = [
+//     'sweater',
+//     'skateboard',
+//     'family-night',
+//         'my room',
+//       ]
+
+// function justCoolStuff(arr1,arr2) {
+//     let arr3 = []
+//     for(let u = 0;u<arr1.length;u++) {
+//     for(let i =0;i<arr2.length;i++) {
+// if(arr1[u] == arr2[i]){
+// arr3.push(arr1[u])
+// }
+//     }
+//     }
+//     console.log(arr3)
+
+
+// }
+
+
+// console.log(justCoolStuff(myStuff, coolStuff));
+
+// //tap3
+
+
+// const meal = [
+//     { name:'arugu', source:'plant' },
+//     { name:'tomatoe', source:'plant'},
+//     { name:'lemon', source:'plant' },
+//     { name:'olive oil', source:'plant' },
+//   ]
+//   const dinner = [
+//     { name:'hamburger', source: 'meat' },
+//     { name:'cheess', source: 'dairy' },
+//     { name:'ketchup', source: 'plant' },
+//     { name: 'bun', source: 'plant' }
+//   ]
+
+// function isTheDinnerVegan(arr) {
+  
+//   for(let i=0;i<arr.length;i++) {
+//     if((arr[i].source == 'meat')) {
+//         return false
+//     }
+//     else {
+//         return true
+//     }
+
+//   }
+// }
+
+//   console.log(isTheDinnerVegan(meal));
+//   console.log(isTheDinnerVegan(dinner));
+
+
+ 
+  
+ //Tap4
+  
+
